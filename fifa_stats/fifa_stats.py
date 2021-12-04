91965c79f927f2433f7efa4b910e263a36d3c1f1
Fergus Miller Kerins <ferggus.mk93@gmail.com>
2021-12-04 17:54:09 +0100

    added top x players function

--- fifa_stats/fifa_stats.py
+++ fifa_stats/fifa_stats.py
@@ -1,14 +1,17 @@
 import pandas as pd
+# from helpers import fifa_file_opener, top_players_in_stat
 import helpers
 
 if __name__ == '__main__':
 
 
     path = 'C:/Users/fergu/PycharmProjects/player_data/'
-    fifa_20_stats = helpers.fifa_file_opener(path, '20')
     fifa_21_stats = helpers.fifa_file_opener(path, '21')
 
-    print(fifa_20_stats.head())
+    top_defenders_21 = helpers.top_players_in_stat(fifa_21_stats, 5, 'defending')  # Calculating top 5 defenders in
+                                                                                   # Fifa 21
+
+
 
 
 
--- fifa_stats/helpers.py
+++ fifa_stats/helpers.py
@@ -3,6 +3,37 @@ import pandas as pd
 
 def fifa_file_opener(path, game_year):
     """
-    Opens Fifa df for the given year
+    Opens fifa df for the given year
+
+    Parameters
+    ----------
+    game_year
+        The fifa version (string)
+    path
+        The path to fifa player stats (string)
+
+    Returns
+    -------
+        pd dataframe of fifa version
     """
-    return pd.read_csv(f'{path}players_{game_year}.csv')
+    return pd.read_csv(f'{path}players_{str(game_year)}.csv')
+
+def top_players_in_stat(fifa_game, no_of_players, stat):
+    """
+    Returns the top x no players in a stat
+
+    Parameters
+    ----------
+    fifa_game
+        The fifa versions players (pd dataframe)
+    no_of_players
+        The number of players to return (int)
+    stat
+        The stat to sort by (string)
+
+    Returns
+    -------
+        pd dataframe with x rows showing top x players in seleceted stat
+    """
+    top_players = fifa_game.sort_values(by=[stat], ascending=False).head(no_of_players)
+    return top_players
