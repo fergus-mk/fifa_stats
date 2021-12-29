68e20b7249b57d53f82968a16d3f0e57581311ab
Fergus Miller Kerins <ferggus.mk93@gmail.com>
2021-12-29 19:10:45 +0100

    added tests for compare editions

--- tests/test_best_players_helpers.py
+++ tests/test_best_players_helpers.py
@@ -2,7 +2,6 @@ import pytest
 import best_players.helpers as helpers
 import best_players.constants as constants
 
-
 fifa_20_stats = helpers.fifa_file_opener(
     constants.PATH, "20"
 )  # Opening df of fifa 20 players
@@ -13,7 +12,7 @@ def test_top_players_in_stat():
     """
     Tests the function helpers.top_players_in_stat
     """
-    best_passers_fifa_20 = helpers.top_players_in_stat(fifa_20_stats, 3, "passing")
-    third_best_player = best_passers_fifa_20["short_name"].iloc[2]
-    expected = "T. Kroos"
+    best_passers_fifa_20 = helpers.top_players_in_stat(fifa_20_stats, 3, 'passing')
+    third_best_player = best_passers_fifa_20['short_name'].iloc[2]
+    expected = 'T. Kroos'
     assert third_best_player == expected
