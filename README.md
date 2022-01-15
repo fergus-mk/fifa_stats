e954996a7f5956a4573e1b3877109b3793cede16
Fergus Miller Kerins <ferggus.mk93@gmail.com>
2022-01-15 14:08:19 +0100

    added comments to functions

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ---
 This project generates insights based on stats from 
-the Fifa series of games. It shows the players with 
-the highest rating of a selected stat from a given
-edition of the Fifa game
+the Fifa series of games.
 
 ## Use
 
@@ -15,10 +13,26 @@ command to initiate the environment:
 
 `conda env create -f stats_env.yml`
 
-To use the project enter the required year, stat and number of players
-when calling the file in a python terminal e.g.,
+To use the project enter the folder player_stats and
+call functions directly from  player_stats.py 
+directly from the command line. Currently there are 
+two options:
 
-`python fifa_stats.py 21 defending 3`
+`python .\compare_eds.py comparer 'M. Salah', 
+'overall',  21, 19`
 
-Would return a bar chart showing  the defending stat of the 3 best defenders 
-in Fifa 21
+Demonstrates use of the 'comparer' function which 
+compares an individual player at a selected stats
+between different editions of the game. It displays
+this via a print statement and the creation of a 
+barplot. In this example the player 'M. Salah' is
+compared between Fifa 21 and Fifa 19.
+
+`python .\compare_eds.py best 'defending', 18, 3`
+
+Demonstrates use of the 'best' function which 
+produces a barplot showing the best players at 
+the selected stat for a given game year. In this
+example the top players at defending in Fifa 18 
+are shown. The final argument, '3' selects how 
+many players to show.
--- compare_editions/helpers.py
+++ compare_editions/helpers.py
@@ -7,24 +7,40 @@ def fifa_file_opener(path, game_year):
 
     Parameters
     ----------
-    game_year
-        The fifa version (string)
     path
-        The path to fifa player stats (string)
+        The path to fifa player stats (str)
+    game_year
+        The fifa version (str)
 
     Returns
     -------
         pd dataframe of fifa version
     """
-    return pd.read_csv(f"{path}/players_{str(game_year)}.csv")  # Chnage to Fifa Players Path
+    return pd.read_csv(f"{path}/players_{str(game_year)}.csv")
 
 
 def stat_selector(player, stat, in_path, year):
     """
     Selects stat for player in game year selected
+
+    Parameters
+    ----------
+    player
+        The player being assessed (str)
+    stat
+        The stat being assessed (str)
+    in_path
+        The path to the folder containing player data (str)
+    year
+        The year of game to look at (int)
+
+    Returns
+    -------
+    stat_selected
+        A number indicating the selected stat value (int)
     """
     df = fifa_file_opener(in_path, year)
-    player_row = df.loc[df['short_name'] == player]
+    player_row = df.loc[df["short_name"] == player]
     stat_selected = int(player_row[stat])
     return stat_selected
 
@@ -32,22 +48,53 @@ def stat_selector(player, stat, in_path, year):
 def stat_comparer(player, stat, stat_value1, stat_value2, year1, year2):
     """
     Compares how a given players selected stat has changed between game editions
+
+    Parameters
+    ----------
+    player
+        The player being assessed (str)
+    stat
+        The stat being assessed (str)
+    stat_value1
+        The stat value in the more recent edition (int)
+    stat_value2
+        The stat value in the less recent edition (int)
+    year1
+        The more recent game year (int)
+    year2
+        The more recent game year (int)
+
+    Returns
+    -------
+        A string describing if the player has improved, stayed the same or got worse
     """
     diff = stat_value1 - stat_value2
     if diff > 0:
-        # add return here instead of print
-        return f'{player} has improved {stat} by {str(diff)} points between fifa{year2} and fifa{year1}'
+        return f"{player} has improved {stat} by {str(diff)} points between fifa{year2} and fifa{year1}"
     elif diff == 0:
-        return f'{player} has stayed the same at {stat} between fifa{year2} and fifa{year1}'
+        return f"{player} has stayed the same at {stat} between fifa{year2} and fifa{year1}"
     elif diff < 0:
-        return f'{player} has got worse at {stat} by {str(diff*-1)} points between fifa{year2} and fifa{year1}'
+        return f"{player} has got worse at {stat} by {str(diff*-1)} points between fifa{year2} and fifa{year1}"
     else:
-        return f'{stat} is not in both game verisons'
+        return f"{stat} is not in both game verisons"
 
 
 def top_players_stat(fifa_df, stat, no_of_players):
     """
-    Add doc string
+    Returns the top x no players in a stat
+
+    Parameters
+    ----------
+    fifa_df
+        The fifa versions players (df)
+    no_of_players
+        The number of players to return (int)
+    stat
+        The stat to sort by (str)
+
+    Returns
+    -------
+        pd df with x rows showing top x players in selected stat
     """
     n_players = int(no_of_players)
     top_players = fifa_df.sort_values(by=[stat], ascending=False).head(n_players)
